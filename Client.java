1046dc3b327054c8850af6e5cc9c48098a64c5f6
efixty <efixty@gmail.com>
2018-01-21 23:45:53 +0400

    ...

--- Client.java
+++ Client.java
@@ -15,29 +15,99 @@ public class Client {
 
 	public static void main(String[] args) {
 		try {
-			while(true){
-				connection = new Socket("localhost", 8080);
-				out = connection.getOutputStream();
-				in = connection.getInputStream();
-				pw = new PrintWriter(out, true);
-				sc = new Scanner(System.in);
-				reader = new BufferedReader(new InputStreamReader(in));
-				new BufferedReader(new InputStreamReader(System.in)).readLine();
-				String input = sc.nextLine();
-				// String input = "4 5";
-				pw.println(input);
-				String answer = reader.readLine();
-				System.out.println(answer);
-			}
+			connection = new Socket("localhost", 2222);
+			
+			new Thread(new InputHandlerOnClient(connection)).start();
+			new Thread(new OutputHandlerOnClient(connection)).start();
+
 		} catch(Exception e) {
-			System.out.println("Something went wrong: client1");
+			System.out.println("Something went wrong: client");
+		}
+	}
+}
+
+class InputHandlerOnClient implements Runnable {
+
+	private Socket connection;
+	private BufferedReader reader;
+	private InputStream in;
+
+	InputHandlerOnClient(Socket connection) {
+		this.connection = connection;
+		try{
+			in = connection.getInputStream();
+		} catch(Exception e) {
+			System.out.println("Problem getting InputStream");
+		}
+		reader = new BufferedReader(new InputStreamReader(in));
+		String input;
+		try {
+			if((input = reader.readLine()).equals(null)) {
+				System.out.println(input);
+			}
+		} catch (Exception e) {
+			System.out.println("something wrong reading, client");
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				String input;
+				if((input = reader.readLine()) != null)
+					System.out.println(input);
+			}
+		} catch (Exception e) {
+			System.out.println("Exception while handling input from server");
+			e.printStackTrace();
 		} finally {
-			pw.flush();
-			pw.close();
 			try {
 				reader.close();
 			} catch(Exception e) {
-				System.out.println("Couldn't close Client1's reader ");
+				System.out.println("Couldn't close InputHandler's reader");
+			}
+		}
+	}
+}
+
+class OutputHandlerOnClient implements Runnable {
+
+	private Socket connection;
+	private PrintWriter pw;
+	private Scanner sc;
+	private OutputStream out;
+
+	OutputHandlerOnClient(Socket connection) {
+		this.connection = connection;
+		try{
+			out = connection.getOutputStream();
+		} catch(Exception e) {
+			System.out.println("Problem getting OutputStream");
+		}
+		pw = new PrintWriter(out, true);
+		sc = new Scanner(System.in);
+	}
+
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				String output;
+				if((output = sc.nextLine()) != null)
+					if(!output.equals("END"))
+						pw.println(output);
+					else { connection.close(); System.exit(-116); }
+			}
+		} catch (Exception e) {
+			System.out.println("Exception while handling output from server");
+			e.printStackTrace();
+		} finally {
+			try {
+				pw.close();
+			} catch(Exception e) {
+				System.out.println("Couldn't close OutputHandler's writer");
 			}
 		}
 	}
--- Server.java
+++ Server.java
@@ -9,57 +9,148 @@ public class Server {
 
 	private static ServerSocket server;
 	private static Socket client;
+	private static List<Socket> clients;
+	private static List<Integer> ids;
+	private static List<String> nicknames;
 
     public static void main(String[] args) throws Exception {
-		
-    	server = new ServerSocket(8080);
+		clients = new ArrayList<Socket>();
+		ids = new ArrayList<Integer>();
+		nicknames = new ArrayList<String>();
+    	server = new ServerSocket(2222);
     	try{
-	    	while(true){
-    			client = server.accept();
+    		while(true) {
+    			do {
+    				client = server.accept();	
+    			} while(checkClient(client));
     			System.out.println(client);
-	    		new Thread(new MyRunnable(client)).start();
-			}			
+				new Thread(new InputHandlerOnServer(client)).start();
+				new Thread(new OutputHandlerOnServer(client)).start();
+			}
     	} catch (Exception e) {
-    		System.out.println("Something went wrong on server");
+    		System.out.println("Something went wrong: server");
+    		e.printStackTrace();
     	}
 
 	}
+
+	private static boolean checkClient(Socket client) {
+		Iterator iterator = clients.iterator();
+		while(iterator.hasNext()) {
+			if(client.equals(iterator.next()))
+				return true;
+		}
+		return false;
+	}
+
+	public synchronized static void addNickname(String nickname) {
+		nicknames.add(nickname);
+	}
+
+	public synchronized static void addId(int id) {
+		ids.add(id);
+	}
+
+	public static int getNicknamesSize() {
+		return nicknames.size();
+	}
+
+	public static int getIdSize() {
+		return ids.size();
+	}
 }
 
-class MyRunnable implements Runnable {
+class InputHandlerOnServer implements Runnable {
 
-	private BufferedReader reader;
-	private String input;
-	private PrintWriter pw;
-	private int a, b;
 	private Socket client;
+	private BufferedReader reader;
+	private InputStream in;
 
-	MyRunnable(Socket client) {
+	InputHandlerOnServer(Socket client) {
 		this.client = client;
+		try{
+			in = client.getInputStream();
+		} catch(Exception e) {
+			System.out.println("Problem getting InputStream");
+		}
+		reader = new BufferedReader(new InputStreamReader(in));
+		String nickname;
+		try{
+			if((nickname = reader.readLine()) != null) {
+				Server.addNickname(nickname);
+			}
+			Integer id;
+			if((id = Integer.parseInt(reader.readLine())) != null) {
+				Server.addId(id);
+			}
+		} catch(Exception e) {
+			System.out.println("Something wrong with reading, server");
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void run() {
 		try {
-			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		  	pw = new PrintWriter(client.getOutputStream(), true);
-		    if((input = reader.readLine()) != null) {
-		    	a = Integer.parseInt(input.split(" ")[0]);
-		    	b = Integer.parseInt(input.split(" ")[1]);
-		    	System.out.println(a + b);
-		    	pw.println(a + b);
-		    	System.out.println("answer sent");
-		    }
-		} catch(Exception e) {
-			System.out.println("Something went wrong in thread");
+			while(true) {
+				String input;
+				if((input = reader.readLine()) != null)
+					System.out.println(input);
+			}
+		} catch (Exception e) {
+			System.out.println("Exception while handling input from server");
 			e.printStackTrace();
 		} finally {
-			pw.flush();
-			pw.close();
 			try {
 				reader.close();
 			} catch(Exception e) {
-				System.out.println("Couldn't close Server's reader");
+				System.out.println("Couldn't close InputHandler's reader");
+			}
+		}
+	}
+}
+
+class OutputHandlerOnServer implements Runnable {
+
+	private Socket client;
+	private PrintWriter pw;
+	private Scanner sc;
+	private OutputStream out;
+
+	OutputHandlerOnServer(Socket client) {
+		this.client = client;
+		try{
+			out = client.getOutputStream();
+		} catch(Exception e) {
+			System.out.println("Problem getting OutputStream");
+		}
+		pw = new PrintWriter(out, true);
+		sc = new Scanner(System.in);
+		String output = "Please choose a nickname (String)";
+		pw.println(output);
+		System.out.println("sent 1st");
+		while(!(Server.getNicknamesSize() == Server.getIdSize())) {
+			pw.println("Please choose an id (int)");
+		}
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				String output;
+				if((output = sc.nextLine()) != null)
+					pw.println(output);
+			}
+		} catch (Exception e) {
+			System.out.println("Exception while handling output from server");
+			e.printStackTrace();
+		} finally {
+			try {
+				pw.close();
+			} catch(Exception e) {
+				System.out.println("Couldn't close OutputHandler's writer");
 			}
 		}
 	}
